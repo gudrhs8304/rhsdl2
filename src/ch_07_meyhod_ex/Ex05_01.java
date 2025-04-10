97c2b79458d9589d31c914806db251ecac4df135
kimhyeonggon <gudrhs8304@gmail.com>
2025-04-10 15:00:20 +0900

    java Edu

--- src/ch_07_meyhod_ex/Ex05_01.java
+++ src/ch_07_meyhod_ex/Ex05_01.java
@@ -2,7 +2,7 @@ package ch_07_meyhod_ex;
 
 import java.util.Scanner;
 
-public class Ex05 {
+public class Ex05_01 {
     /*
     -----------------------------------------------------
     1.계좌생성 | 2.입금 | 3.출금 | 4.잔액조회 | 5.종료
@@ -52,6 +52,7 @@ public class Ex05 {
     static Scanner scanner = new Scanner(System.in);
     static int balance = -1;
     static int money;
+
     public static void main(String[] args) {
 
         boolean flag = true; // 반복문의 조건으로 사용 -> 값이 false가 된다면 반복문이 종료.
@@ -61,16 +62,21 @@ public class Ex05 {
             System.out.println("-----------------------------------------------------");
             System.out.print("선택> ");
             int selectNo = scanner.nextInt();
-            if (selectNo == 1) {
-                balance = createAccount(balance);
-            } else if (selectNo == 2) {
-                inputDeposit();
-            } else if (selectNo == 3) {
-                printWithdrawal();
-            } else if (selectNo == 4) {
-                System.out.println("현재 잔액: " + balance);
-            } else if (selectNo == 5) {
-                flag = setRun();
+            switch (selectNo) {
+                case 1:
+                    balance = createAccount(balance);
+                    break;
+                case 2:
+                    balance = inputDeposit(balance);
+                    break;
+                case 3:
+                    balance = printWithdrawal(balance);
+                    break;
+                case 4:
+                    System.out.println("현재 잔액: " + balance);
+                    break;
+                case 5:
+                    flag = setRun();
 
             }
         }
@@ -86,28 +92,31 @@ public class Ex05 {
         return balance;
     }
 
-    static void inputDeposit() {
+    static int inputDeposit(int balance) {
         if (balance == -1) {
             System.out.println("계좌를 먼저 생성해 주세요.");
-            return;
+            return balance;
         }
         if (balance >= 0) {
             System.out.print("입금액> ");
             balance += scanner.nextInt();
             System.out.println("입금 완료. 현재 잔액: " + balance + "원");
+            return balance;
 
         }
+        return balance;
     }
 
-    static void printWithdrawal() {
+    static int printWithdrawal(int balance) {
         System.out.print("출금액> ");
         money = scanner.nextInt();
         if (money > balance) {
             System.out.println("잔액이 부족합니다.");
-            return;
+            return balance;
         }
         balance -= money;
         System.out.println("현재 잔액: " + balance);
+        return balance;
     }
 
     static boolean setRun() {
